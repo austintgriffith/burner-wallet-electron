e6a53f982fae44e8b88dd1104595a941b455a7e7
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-01-21 23:48:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,76 +3,6 @@ const path = require("path");
 
 const { app, BrowserWindow, Menu } = electron;
 
-const template = [
-  {
-    label: "Edit",
-    submenu: [
-      { role: "undo" },
-      { role: "redo" },
-      { type: "separator" },
-      { role: "cut" },
-      { role: "copy" },
-      { role: "paste" },
-      { role: "pasteandmatchstyle" },
-      { role: "delete" },
-      { role: "selectall" }
-    ]
-  },
-  {
-    label: "View",
-    submenu: [
-      { role: "reload" },
-      { role: "forcereload" },
-      { role: "toggledevtools" },
-      { type: "separator" },
-      { role: "resetzoom" },
-      { role: "zoomin" },
-      { role: "zoomout" },
-      { type: "separator" },
-      { role: "togglefullscreen" }
-    ]
-  },
-  {
-    role: "window",
-    submenu: [{ role: "minimize" }, { role: "close" }]
-  }
-];
-
-if (process.platform === "darwin") {
-  template.unshift({
-    label: app.getName(),
-    submenu: [
-      { role: "about" },
-      { type: "separator" },
-      { role: "services", submenu: [] },
-      { type: "separator" },
-      { role: "hide" },
-      { role: "hideothers" },
-      { role: "unhide" },
-      { type: "separator" },
-      { role: "quit" }
-    ]
-  });
-
-  // Edit menu
-  template[1].submenu.push(
-    { type: "separator" },
-    {
-      label: "Speech",
-      submenu: [{ role: "startspeaking" }, { role: "stopspeaking" }]
-    }
-  );
-
-  // Window menu
-  template[3].submenu = [
-    { role: "close" },
-    { role: "minimize" },
-    { role: "zoom" },
-    { type: "separator" },
-    { role: "front" }
-  ];
-}
-
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
@@ -84,13 +14,25 @@ app.on("ready", function() {
     width: 1024,
     height: 768,
     icon: path.join(__dirname, "assets/icons/png/64x64.png"),
-    frame: false
+    frame: true
   });
   mainWindow.loadURL("https://xdai.io");
   // Display Dev Tools by default
   //mainWindow.openDevTools();
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+
+  // Prompt users before window close
+  mainWindow.on("close", e => {
+    // e.preventDefault();
+  });
+
+  // Emitted when the window is closed.
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
 });
 
 // Quit when all windows are closed.
